1d18e104046b7b90774d942495ae82181b1ad092
shoaib8236 <shoaib.raza.dev@gmail.com>
2021-09-03 18:51:09 +0500

    Admin Login Page Designed

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,43 +1,41 @@
 import React, { Component } from "react";
 import Sidebar from "../Components/Sidebar/Sidebar";
-import {getToken, setToken} from "../utils/Auth";
+import { getToken, setToken } from "../utils/Auth";
 import Header from "../Components/Header/Header";
 
-let Layout = ComposedComponent => class extends Component {
+let Layout = (ComposedComponent) =>
+  class extends Component {
     constructor(props) {
-        super(props);
-        this.state={
-            token: getToken(),
-        }
+      super(props);
+      this.state = {
+        token: getToken(),
+      };
     }
-
 
     componentWillMount() {
-        this.navigateToLogIn()
+      this.navigateToLogIn();
     }
 
-    navigateToLogIn(){
-        let token = getToken();
-        console.log('token===>',token)
-        !token && this.props.history.push('/signIn')
+    navigateToLogIn() {
+      let token = getToken();
+      console.log("token===>", token);
+      !token && this.props.history.push("/signIn");
     }
 
     render() {
-        let {token} = this.state
-        return(
-            <div className="main-layout">
-                {token?<Header /> : null}
-                <div className="main-content">
-                    {token?<Sidebar /> : null}
-                    <div className="content">
-                        <ComposedComponent {...this.props} {...this.state} />
-                    </div>
-                </div>
+      let { token } = this.state;
+      return (
+        <div className="main-layout">
+          {token ? <Header /> : null}
+          <div className="main-content">
+            {token ? <Sidebar /> : null}
+            <div className="content">
+              <ComposedComponent {...this.props} {...this.state} />
             </div>
-        )
+          </div>
+        </div>
+      );
     }
-};
+  };
 
 export default Layout;
-
-
